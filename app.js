1e34eec7fd02380d8e5b18a78065f961b5832cbd
VirakSy <virak.sy@cezium.fr>
2022-02-08 14:48:45 +0100

    specify node & npm version

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser');
-const fs = require('fs');
-const http = require('http');
-const https = require('https');
-const privateKey = fs.readFileSync('key.pem', 'utf8');
-const certificate = fs.readFileSync('cert.pem', 'utf8');
-const credentials = { key: privateKey, cert: certificate };
+const bodyParser = require('body-parser')
 
 const submodules = [
     require('./modules/discount-code/app/app'),
@@ -16,8 +10,9 @@ const submodules = [
 const app = express();
 
 // parse application/json
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
+app.set('port', (process.env.PORT || 8080));
 app.use('/', express.static(path.join(__dirname, 'home')));
 app.use('/assets', express.static(path.join(__dirname, '/node_modules/@salesforce-ux/design-system/assets')));
 
@@ -25,19 +20,6 @@ submodules.forEach((sm) => sm(app, {
     rootDirectory: __dirname,
 }));
 
-// app.listen(app.get('port'), function () {
-//     console.log(`Express is running at localhost: ${app.get('port')}`);
-// });
-
-const httpServer = http.createServer(app);
-const httpsServer = https.createServer(credentials, app);
-
-const httpPort = process.env.PORT || 8443;
-httpServer.listen(httpPort, function () {
-    console.log(`Express is running at localhost: ${httpPort}`);
-});
-
-const httpsPort = process.env.PORT || 443;
-httpsServer.listen(httpsPort, function () {
-    console.log(`Express is running at localhost: ${httpsPort}`);
+app.listen(app.get('port'), function () {
+    console.log(`Express is running at localhost: ${app.get('port')}`);
 });
